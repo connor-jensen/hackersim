6e2a3b112ef1f36040c3c0b8843a0033318810f6
bcis93 <bryancisneros93@gmail.com>
2018-10-19 03:50:58 +0000

    added sound functionality and randomized special keys

--- script.js
+++ script.js
@@ -1,74 +1,87 @@
 var app = angular.module('HackerSim', []);
 
 app.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
-    
-    var speed = 10;
-    var content = '';
-    $scope.codingText = '';
-    $scope.cursor = '|';
-    $http.get('code.txt').then(function(file) {
-      content = file.data;
-      console.log(file);
-    })
-    
-    setInterval(function(){removeNotifications()}, 8000)
-    
-    function removeNotifications() {
-      var elements = document.getElementsByClassName("notification");
-      while(elements.length > 0){
-          elements[0].parentNode.removeChild(elements[0]);
-      }
+
+  var speed = 10;
+  var content = '';
+  $scope.codingText = '';
+  $scope.cursor = '|';
+  $http.get('code.txt').then(function(file) {
+    content = file.data;
+    console.log(file);
+  })
+
+  setInterval(function() { removeNotifications() }, 8000)
+
+  function removeNotifications() {
+    var elements = document.getElementsByClassName("notification");
+    while (elements.length > 0) {
+      elements[0].parentNode.removeChild(elements[0]);
     }
-    
-    $scope.hack = function(event) {
-      if(event.key === "+") {
-        speed = 30;
-      }
-      else if(event.key === "-" && speed > 10) {
-        speed = 5;
-      }
-      
-      if(event.key === "1") {
+  }
+
+  $scope.hack = function(event) {
+    if (event.key === "+") {
+      speed = 30;
+    }
+    else if (event.key === "-" && speed > 10) {
+      speed = 5;
+    }
+
+    if (event.key > '0' && event.key <= '5') {
+      var action = Math.floor(Math.random() * 5 + 1);
+      if (action === 1) {
         removeNotifications();
         var success = angular.element("<div>ACCESS GRANTED</div>");
         success.addClass("good");
         success.addClass("notification");
         angular.element(document.body).prepend(success);
+        var audio = new Audio('sounds/access_granted.wav');
+        audio.play();
       }
-      else if(event.key === "2") {
+      else if (action === 2) {
         removeNotifications();
         var denied = angular.element("<div>ACCESS DENIED</div>");
         denied.addClass("bad");
         denied.addClass("notification");
         angular.element(document.body).prepend(denied);
+        var audio = new Audio('sounds/access_denied.wav');
+        audio.play();
       }
-      else if(event.key === "3") {
+      else if (action === 3) {
         removeNotifications();
         var bitcoin = angular.element("<div>BITCOIN MINED</div>");
         bitcoin.addClass("good");
         bitcoin.addClass("notification");
         angular.element(document.body).prepend(bitcoin);
+        var audio = new Audio('sounds/bitcoin_mined.wav');
+        audio.play();
       }
-      else if(event.key === "4") {
+      else if (action === 4) {
         removeNotifications();
         var mainframe = angular.element("<div>HACKING INTO THE MAINFRAME</div>");
         mainframe.addClass("good");
         mainframe.addClass("notification");
         angular.element(document.body).prepend(mainframe);
+        var audio = new Audio('sounds/hacking_into_mainframe.wav');
+        audio.play();
       }
-      else if(event.key === "5") {
+      else if (action === 5) {
         removeNotifications();
         var detected = angular.element("<div>HACKING DETECTED!</div>");
         detected.addClass("bad");
         detected.addClass("notification");
         angular.element(document.body).prepend(detected);
+        var audio = new Audio('sounds/hacking_detected.wav');
+        audio.play();
       }
-      else if(event.key === "]") {
-        removeNotifications();
-      }
-      else if ($scope.codingText.length < content.length) {
-        $scope.codingText += content.substr($scope.codingText.length, speed);
-        window.scrollBy(0,50);
-      } 
-    };
+    }
+    else if (event.key === "]") {
+      removeNotifications();
+    }
+    else if ($scope.codingText.length < content.length) {
+      $scope.codingText += content.substr($scope.codingText.length, speed);
+      window.scrollBy(0, 50);
+    }
+  };
 }]);
